540f367b07909b8b1fa61adf76519a9d051117a9
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-03-04 18:09:41 -0700

    fixed attack buton

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,153 @@
 $(document).ready(function () {
     var userPick = false;
     var defender = false;
+
     var batman = {
+        name: "batman",
         healthPoints: 100,
         attackPower: 8,
-        cntrAtkPwr: 5
+        cntrAtkPwr: 5,
+        increment: 8
     };
     var catwoman = {
+        name: "catwoman",
         healthPoints: 120,
         attackPower: 6,
-        cntrAtkPwr: 4
+        cntrAtkPwr: 4,
+        increment: 6
     };
     var joker = {
+        name: "joker",
         healthPoints: 180,
         attackPower: 6,
-        cntrAtkPwr: 20
+        cntrAtkPwr: 20,
+        increment: 6
     };
     var rasAlGhul = {
+        name: "ras-al-ghul",
         healthPoints: 150,
         attackPower: 10,
-        cntrAtkPwr: 6
+        cntrAtkPwr: 6,
+        increment: 10
     };
-    var leftDiv;
-    var rightDiv;
 
+    var leftDiv = {};
+    var rightDiv = {};
 
+    $("#batman #health").text(batman.healthPoints);
+    $("#catwoman #health").text(catwoman.healthPoints);
+    $("#joker #health").text(joker.healthPoints);
+    $("#ras-al-ghul #health").text(rasAlGhul.healthPoints);
 
 
     // move players to positions
-    $(".image").on("click", function () {
+    // tried this first, but couldn't figure out how to assign the array properties in a general manner
+    // $(".image").on("click", function () {
+    //     if (userPick === false) {
+    //         $(this).appendTo("#left");
+    //         userPick = true;
+    //         leftDiv = $(this).attr("id");
+    //         console.log(leftDiv);
+    //     } else {
+    //         $(this).appendTo("#right");
+    //         defender = true;
+    //         rightDiv = $(this).attr("id");
+    //         console.log(rightDiv);
+    //     };
+    //     if (defender === true) {
+    //         $(".image").off("click");
+    //     };
+    // });
+
+    //went with separate functions for each image to 
+    $("#batman").on("click", function () {
         if (userPick === false) {
             $(this).appendTo("#left");
             userPick = true;
-            leftDiv = $(this).attr("id");
+            leftDiv = batman;
+            $("#batman").off("click");
             console.log(leftDiv);
         } else {
             $(this).appendTo("#right");
             defender = true;
-            rightDiv = $(this).attr("id");
+            rightDiv = batman;
             console.log(rightDiv);
         };
         if (defender === true) {
             $(".image").off("click");
         };
-        if (leftDiv === "batman") {
-
-            console.log(batman.healthPoints);
+    });
+    $("#catwoman").on("click", function () {
+        if (userPick === false) {
+            $(this).appendTo("#left");
+            userPick = true;
+            leftDiv = catwoman;
+            $("#catwoman").off("click");
+            console.log(leftDiv);
+        } else {
+            $(this).appendTo("#right");
+            defender = true;
+            rightDiv = catwoman;
+            console.log(rightDiv);
+        };
+        if (defender === true) {
+            $(".image").off("click");
+        };
+    });
+    $("#joker").on("click", function () {
+        if (userPick === false) {
+            $(this).appendTo("#left");
+            userPick = true;
+            leftDiv = joker;
+            $("#joker").off("click");
+            console.log(leftDiv);
+        } else {
+            $(this).appendTo("#right");
+            defender = true;
+            rightDiv = joker;
+            console.log(rightDiv);
+        };
+        if (defender === true) {
+            $(".image").off("click");
+        };
+    });
+    $("#ras-al-ghul").on("click", function () {
+        if (userPick === false) {
+            $(this).appendTo("#left");
+            userPick = true;
+            leftDiv = rasAlGhul;
+            $("#ras-al-ghul").off("click");
+            console.log(leftDiv);
+        } else {
+            $(this).appendTo("#right");
+            defender = true;
+            rightDiv = rasAlGhul;
+            console.log(rightDiv);
+        };
+        if (defender === true) {
+            $(".image").off("click");
         };
     });
 
 
-    // assign `Health Points`, `Attack Power` and `Counter Attack Power` to each player
+
+    $("#attack").on("click", function () {
+        leftDiv.healthPoints -= rightDiv.cntrAtkPwr;
+        leftDiv.attackPower += leftDiv.increment;
+        rightDiv.healthPoints -= leftDiv.attackPower;
+        $("#" + leftDiv.name + " #health").text(leftDiv.healthPoints);
+        $("#" + rightDiv.name + " #health").text(rightDiv.healthPoints);
+        console.log(leftDiv, rightDiv);
+    });
+
+
+    // need to assign individual healthPoints to each respective image div
+    // need to assign and make active the attackPower and ctrAtkPwr for each image div
+    // need to make it so on attack the left side = healthPoints - ctrAtkPwr and the right side is healthPoints - attackPower
+    // display new healthPoints on each side
+    // when healthPoints <= 0, then player loses or defender dissapears
+    // attackPower continues to increase by the starting amount throughout the game
+    // 
 
 
 
