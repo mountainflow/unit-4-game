bc9156dff47308f752a9d9bc4987b496bb3f974c
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-03-04 23:09:22 -0700

    not finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,6 +30,41 @@ $(document).ready(function () {
         cntrAtkPwr: 6,
         increment: 10
     };
+    // var characterObj = {
+    //     batman: {
+    //         name: "batman",
+    //         healthPoints: 100,
+    //         attackPower: 8,
+    //         cntrAtkPwr: 5,
+    //         increment: 8
+    //     },
+    //     catwoman: {
+    //         name: "catwoman",
+    //         healthPoints: 120,
+    //         attackPower: 6,
+    //         cntrAtkPwr: 4,
+    //         increment: 6
+    //     },
+    //     joker: {
+    //         name: "joker",
+    //         healthPoints: 180,
+    //         attackPower: 6,
+    //         cntrAtkPwr: 20,
+    //         increment: 6
+    //     },
+    //     'ras-al-ghul': {
+    //         name: "ras-al-ghul",
+    //         healthPoints: 150,
+    //         attackPower: 10,
+    //         cntrAtkPwr: 6,
+    //         increment: 10
+    //     }
+    // };
+    // for (key in characterObj) {
+    // var characterDiv = $("<div>...")
+    // build up divs to append to one another
+    // append characterDiv to main div in html
+    // }
 
     var leftDiv = {};
     var rightDiv = {};
@@ -138,6 +173,18 @@ $(document).ready(function () {
         $("#" + leftDiv.name + " #health").text(leftDiv.healthPoints);
         $("#" + rightDiv.name + " #health").text(rightDiv.healthPoints);
         console.log(leftDiv, rightDiv);
+        // leftDiv health points goes to zero -> game over
+        if (leftDiv.healthPoints <= 0) {
+            // have to change text in scoring sections
+            // reset entire game
+            console.log('you lose')
+        }
+        // rightDiv goes to zero; user has to select another opponent/win
+        if (rightDiv.healthPoints <= 0) {
+            // disable image - get rid or fade image elsewhere
+            // next opponent is clickable (reset of next opponents; defender = false)
+            console.log('next opponent/win')
+        }
     });
 
 
